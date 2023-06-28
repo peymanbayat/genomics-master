00bfe2b67918dd52b597126a5297f9d6700b42ae
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-06-28 17:53:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -15,64 +15,78 @@ pickle_in = open('Diabetes.pkl', 'rb')
 classifier = pickle.load(pickle_in)
 
 def predict():
-    st.sidebar.header('GenomicsMaster Co.')
+    st.sidebar.header('IntelBioMax Co.')
     # select = st.sidebar.selectbox('Select Form', ['Form 1'], key='1')
     # if not st.sidebar.checkbox("Hide", True, key='2'):
-    st.title('GenomicsMaster Prediction Product')
-    st.markdown('This trained dataset is originally collected by GenomicsMaster Company.')
-    st.markdown('A Genome-Base Tool to Predict Future Possible Illnesses')
+    st.title('IntelBioMax (Suitable Type of Antibiotic Prediction)')
+    st.markdown('This trained dataset is originally collected by IntelBioMax Company.')
+    st.markdown('It will be very greatful if you share your data in this regard with us.')
 
-    name = st.text_input("Name:")
-    pregnancy = st.number_input("CHROM:")
-    st.markdown('Chromosome the variant is located on')
+    name = st.text_input("Patient's Name (Optional)")
+    pregnancy = st.number_input("Please connect the device to your computer, Data Reading from the Devics:")
+    st.markdown('Number of time periods consideration in the plot (read from device)')
+    
+    glucose = st.number_input("Plasma Glucose Concentration :")
+    st.markdown('Glucose: Plasma glucose concentration a 2 hours in an oral glucose tolerance test')
 
-    glucose = st.number_input("POS:")
-    st.markdown('Position on the chromosome the variant is located on')
+    bp =  st.number_input("Diastolic blood pressure (mm Hg):")
+    st.markdown('BloodPressure: Diastolic blood pressure (mm Hg)')
 
-    bp =  st.number_input("REF:")
-    st.markdown('Reference Allele')
+    skin = st.number_input("Triceps skin fold thickness (mm):")
+    st.markdown('SkinThickness: Triceps skin fold thickness (mm)')
 
-    skin = st.number_input("ALT:")
-    st.markdown('Alternaete Allele')
-
-    insulin = st.number_input("AF_ESP:")
-    st.markdown('Allele frequencies from GO-ESP')
-
-
-    bmi = st.number_input("CLNDISDB:")
-    st.markdown('Tag-value pairs of disease database name and identifier')
-
-    dpf = st.number_input("CLNDISDBINCL:")
-    st.markdown('For included Variant: Tag-value pairs of disease database name and identifier')
+    insulin = st.number_input("2-Hour serum insulin (mu U/ml):")
+    st.markdown('Insulin: 2-Hour serum insulin (mu U/ml)')
 
 
-    age = st.number_input("CLNDN:")
-    st.markdown('Preferred ClinVar disease name for the concept specified by disease identifiers in CLNDISDB')
+    bmi = st.number_input("Body mass index (weight in kg/(height in m)^2):")
+    st.markdown('BMI: Body mass index (weight in kg/(height in m)^2)')
 
+    
+    
+   
+   
+    dpf = st.selectbox(
+    'Antibiotic Types',
+    ('1: "Penicillins"', '2: "Cephalosporins"', '3: "Tetracyclines"', '4:"Aminoglycosides"'))
+    st.write('You selected:', dpf)
+
+    if dpf == '1: "Penicillins"':
+        dpf = 1
+    if dpf == '2: "Cephalosporins"':
+        dpf = 2
+    if dpf == '3: "Tetracyclines"':
+        dpf = 3
+    if dpf == '4: "Aminoglycosides"':
+        dpf =4
+    
+
+        
+
+    age = st.number_input("Age:")
+    st.markdown('Age: Age (years)')
 
     submit = st.button('Predict')
     st.markdown('Outcome: Class variable (0 or 1)')
 
-
     if submit:
         prediction = classifier.predict([[pregnancy, glucose, bp, skin, insulin, bmi, dpf, age]])
         if prediction == 0:
-            st.write('Congratulation!',name,'The Probability of Your Illness is Very Low')
+            st.write('Congratulation!', name,'It seems you have selected a fit type of Antibiotic')
         else:
-            st.write(name,", The Probability of Your Ilness is Relatively High")
-             
+            st.write(name,", It seems you can choose a more fitted Antibiotic")
 
 def main():
-    new_title = '<p style="font-size: 42px;">Welcome Ilnesses Possibility Prediction App!</p>'
+    new_title = '<p style="font-size: 42px;">Welcome The Congenital Disabilities Prediction App!</p>'
     read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
     read_me = st.markdown("""
     The application is built using Streamlit  
+    to demonstrate Fit Type of Antibiotic Prediction. It performs prediction on multiple parameters
                                   """)
-    
     st.sidebar.title("Select Activity")
     choice = st.sidebar.selectbox(
-        "MODE", ("About", "Predict Future Possible Illnesses"))
-    if choice == "Predict Illness":
+        "MODE", ("About", "Predict Antibiotic Type Effects"))
+    if choice == "Predict Antibiotic Type Effects":
         read_me_0.empty()
         read_me.empty()
         predict()
@@ -82,3 +96,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
