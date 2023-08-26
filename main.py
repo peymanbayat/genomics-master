99a9362d3c5e5e91bb3bb12690e59e2301ee93cb
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-08-26 15:40:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 import pickle
 
+
 pickle_in = open('Diabetes.pkl', 'rb')
 classifier = pickle.load(pickle_in)
 
